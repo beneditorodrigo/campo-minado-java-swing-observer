f5cd2551f7347cc794ba762fae46dd8ec1fb9fe3
“BeneditoRodrigo <rodrigodacunha7@gmail.com>
2022-06-14 12:30:10 -0300

    Criação da janela principal

---
+++ src/br/com/beneditorodrigo/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.beneditorodrigo.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.beneditorodrigo.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel{
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
